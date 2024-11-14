5a059db0c3873c7f7f57b80ca51665f1241be232
BrandLuna <85650103+BrandLuna@users.noreply.github.com>
2024-11-13 20:24:57 -0500

    Primeros cambios

---
+++ cypress/e2e/tests/addcart.cy.js
@@ -0,0 +1,29 @@
+//Prueba
+//Agregar un elemento al carrito de compras usando Page Object Model
+
+import InventarioPage from '../pages/InventarioPage';
+import LoginPage from '../pages/loginPage';
+
+describe('Prueba de Login usando POM', () => {
+    beforeEach(function () {
+        // Carga el fixture de datos de usuario
+        cy.fixture('usuarios').as('usuariosData');
+    });
+
+    it('Login con usuario estándar', function () {
+        // Visita la página de login
+        LoginPage.visit();
+
+        // Usa datos del fixture para hacer login
+        const usuario = this.usuariosData.usuario1;
+        LoginPage.login(usuario.username, usuario.password)
+        cy.url().should('eq', 'https://www.saucedemo.com/v1/inventory.html');
+
+        // InventarioPage.clickaddtocart2();
+        // InventarioPage.validarcarritoElemento1();
+
+
+
+
+    });
+});
